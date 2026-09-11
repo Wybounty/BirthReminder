8c13d6724798da3c52b5bd6be0b68c69ef2f4175
wybounty <siberfrancis971@gmail.com>
2026-09-10 22:51:17 -0400

    Setup project

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,7 +12,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 /**
  * @property int $id
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\HomeController;
-
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
 
-    //Route::inertia('/dashboard', 'dashboard')->name('dashboard');
+    // Route::inertia('/dashboard', 'dashboard')->name('dashboard');
 });
 
 require __DIR__.'/settings.php';
